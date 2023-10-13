ab5ad8d5534d5367c43bddd157a3c8d63f0b33b1
Yamila Rampello <99457830+YamiRampello@users.noreply.github.com>
2023-10-13 11:15:50 -0300

    Agrega tarea 1 de la clase 10
    A la tarea 1 de la clase 6 se agregan estilos con bootstrap. Se trabajó sobre la solución de Fabricio Sodano explicada en clase.

---
+++ clase-10/clase-6-tarea-1-FS.js
@@ -0,0 +1,209 @@
+/*
+TAREA: Empezar preguntando cuánta gente hay en el grupo familiar.
+Crear tantos inputs+labels como gente haya para completar la edad de cada integrante.
+Al hacer click en "calcular", mostrar en un elemento pre-existente la mayor edad,
+la menor edad y el promedio del grupo familiar.
+
+Punto bonus: Crear un botón para "empezar de nuevo" que empiece el proceso nuevamente,
+ borrando los inputs ya creados (investigar cómo en MDN).
+*/
+
+const $form = document.querySelector('#calculador-edades');
+
+document.querySelector('#siguiente-paso').onclick = function (event) {
+  const identificaError = 'cantidadIntegrantes';
+  const $cantidadIntegrantes = document.querySelector('#cantidad-integrantes');
+  const cantidadIntegrantes = Number($cantidadIntegrantes.value);
+  const errorCantidadIntegrantes =
+    validarCantidadIntegrantes(cantidadIntegrantes);
+  const errores = {
+    'cantidad-integrantes': errorCantidadIntegrantes //asi coincide con el name de html
+  };
+  const mensajesErrores = document.querySelector('ul');
+  mensajesErrores.innerHTML = '';
+
+  manejarErrores(errores, identificaError);
+  borrarIntegrantesAnteriores();
+
+  if (errorCantidadIntegrantes === '') {
+    crearIntegrantes(cantidadIntegrantes);
+  } else {
+    ('');
+  }
+  event.preventDefault();
+};
+
+document.querySelector('#calcular').onclick = function (event) {
+  const identificaError = 'calcularEdades';
+  const numeros = obtenerEdadesIntegrantes();
+  const errorEdad = guardarErrores(numeros);
+  const cantidadErrores = contarError(numeros);
+
+  const errores = {
+    integrante: errorEdad //asi coincide con el name de html
+  };
+
+  const mensajesErrores = document.querySelector('ul');
+  mensajesErrores.innerHTML = '';
+
+  if (cantidadErrores === 0) {
+    mostrarEdad('mayor', obtenerMayorNumero(numeros));
+    mostrarEdad('menor', obtenerMenorNumero(numeros));
+    mostrarEdad('promedio', obtenerPromedio(numeros));
+    mostrarResultados();
+  } else {
+    manejarErrores(errores, identificaError);
+  }
+
+  event.preventDefault();
+};
+
+function contarError(numeros) {
+  let contadorErorr = 0;
+
+  for (let i = 0; i < numeros.length; i++) {
+    if (validarEdad(numeros[i]) !== '') {
+      contadorErorr++;
+    } else {
+      ('');
+    }
+  }
+
+  return contadorErorr;
+}
+
+function guardarErrores(numeros) {
+  let errorEdad = [];
+  for (let i = 0; i < numeros.length; i++) {
+    errorEdad.push(validarEdad(numeros[i]));
+  }
+  return errorEdad;
+}
+
+function manejarErrores(errores, identificaError) {
+  const keys = Object.keys(errores);
+  const $errores = document.querySelector('#errores');
+
+  keys.forEach(function (key) {
+    const error = errores[key];
+
+    if (identificaError === 'cantidadIntegrantes') {
+      if (error === '') {
+        $form[key].className = '';
+      } else {
+        $form[key].className = 'error';
+        const $error = document.createElement('li');
+        $error.innerText = error;
+        $errores.appendChild($error);
+      }
+    } else {
+      for (let i = 0; i < error.length; i++) {
+        if (error[i] === '') {
+          $form[key][i].className = '';
+        } else {
+          $form[key][i].className = 'error';
+          const $error = document.createElement('li');
+          $error.innerText = error[i];
+          $errores.appendChild($error);
+        }
+      }
+    }
+  });
+}
+
+document.querySelector('#resetear').onclick = resetear;
+
+function borrarIntegrantesAnteriores() {
+  const $integrantes = document.querySelectorAll('.integrante');
+  for (let i = 0; i < $integrantes.length; i++) {
+    $integrantes[i].remove();
+  }
+}
+
+function crearIntegrantes(cantidadIntegrantes) {
+  if (cantidadIntegrantes > 0) {
+    mostrarBotonCalculo();
+  } else {
+    resetear();
+  }
+
+  for (let i = 0; i < cantidadIntegrantes; i++) {
+    crearIntegrante(i);
+  }
+}
+
+function crearIntegrante(indice) {
+  const $div = document.createElement('div');
+  $div.className = 'integrante';
+
+  const $label = document.createElement('label');
+  $label.textContent = 'Edad del integrante #: ' + (indice + 1);
+
+  const $input = document.createElement('input');
+  $input.type = 'number';
+  $input.name = 'integrante';
+  $input.class = 'form-control form-control-lg';
+
+  $div.appendChild($label);
+  $div.appendChild($input);
+
+  const $integrantes = document.querySelector('#integrantes');
+  $integrantes.appendChild($div);
+}
+
+function resetear() {
+  borrarIntegrantesAnteriores();
+  ocultarBotonCalculo();
+  ocultarResultados();
+}
+
+function ocultarBotonCalculo() {
+  document.querySelector('#calcular').className = 'oculto';
+}
+
+function mostrarBotonCalculo() {
+  document.querySelector('#calcular').className = '';
+}
+
+function ocultarResultados() {
+  document.querySelector('#analisis').className = 'oculto';
+}
+
+function mostrarResultados() {
+  document.querySelector('#analisis').className = '';
+}
+
+function mostrarEdad(tipo, valor) {
+  document.querySelector(`#${tipo}-edad`).textContent = valor;
+}
+
+function obtenerEdadesIntegrantes() {
+  const $integrantes = document.querySelectorAll('.integrante input');
+  const edades = [];
+  for (let i = 0; i < $integrantes.length; i++) {
+    edades.push(Number($integrantes[i].value));
+  }
+  return edades;
+}
+
+function validarCantidadIntegrantes(cantidadIntegrantes) {
+  if (cantidadIntegrantes < 0) {
+    return 'En este campo debe ingresar un número positivo';
+  }
+
+  if (!/^[0-9]{1,2}$/.test(cantidadIntegrantes)) {
+    return 'En este campo debe ingresar un numero entero entre 1 y 2 digitos';
+  }
+  return '';
+}
+
+function validarEdad(edad) {
+  if (edad < 0) {
+    return 'En este campo debe ingresar un número positivo';
+  }
+
+  if (!/^[0-9]{1,3}$/.test(edad)) {
+    return 'En este campo debe ingresar un numero entero entre 1 y 3 digitos';
+  }
+  return '';
+}
